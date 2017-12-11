43e837e4fac891ba3c9c52a47db5ca7a525d3eac
Lenny Sirivong <lenny@lenny-s.com>
2017-12-11 14:26:06 -0500

    Simple gatsby sample page hierarchy

---
+++ src/pages/page-2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>PAGE 2</h1>
+    <p>EDIT</p>
+    <div>
+      <Link to="/page-3/">Page 3</Link>
+    </div>
+  </div>
+)
+
+export default SecondPage
---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div style={{ background: '#fcc' }}>
+    <h1>PAGE 3</h1>
+    <p>Welcome to Page 3 AAAAAAA</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
